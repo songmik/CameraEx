e1d6823523b95b8ed5bfc5f40c9fe338c067e047
Songmi <ksongmi0727@gmail.com>
2022-03-16 22:31:42 +0900

    0316

--- app/src/main/java/com/example/a25_camera/Application.kt
+++ app/src/main/java/com/example/a25_camera/Application.kt
@@ -1,10 +1,8 @@
 package com.example.a25_camera
 
-import androidx.camera.core.CameraX
+import androidx.camera.camera2.Camera2Config
 import androidx.camera.core.CameraXConfig
 
 class Application: Application(), CameraXConfig.Provider {
-    override fun getCameraXConfig(): CameraXConfig {
-        TODO("Not yet implemented")
-    }
+    override fun getCameraXConfig(): CameraXConfig = Camera2Config.defaultConfig()
 }
