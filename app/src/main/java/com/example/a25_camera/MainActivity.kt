d7c2e3e21f78468f6605c3adb4ddd10b423bd16d
Songmi <ksongmi0727@gmail.com>
2022-03-19 13:37:14 +0900

    0319

--- app/src/main/java/com/example/a25_camera/MainActivity.kt
+++ app/src/main/java/com/example/a25_camera/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.a25_camera
 
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.hardware.display.DisplayManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -12,6 +15,21 @@ import com.example.a25_camera.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+    private val displayManager by lazy {
+        getSystemService(Context.DISPLAY_SERVICE) as DisplayManager
+    }
+    private val displayListener = object  : DisplayManager.DisplayListener{
+        override fun onDisplayAdded(p0: Int) = Unit
+
+        override fun onDisplayRemoved(p0: Int) = Unit
+
+        override fun onDisplayChanged(p0: Int) {
+            if (this@MainActivity.displayId == displayId) {
+
+            }
+        }
+
+    }
 
     private val cameraProviderFuture by lazy { ProcessCameraProvider.getInstance(this) }
 
@@ -36,11 +54,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun startCamera(viewFinder: PreviewView){
-
+        displayManager.registerDisplayListener()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+
+        if( requestCode == REQUEST_CODE_PERMISSIONS) {
+            startCamera(binding.viewFinder)
+        } else {
+            Toast.makeText(this, "카메라 권한이 없습니다.", Toast.LENGTH_SHORT).show()
+            finish()
+        }
     }
 
     companion object {
